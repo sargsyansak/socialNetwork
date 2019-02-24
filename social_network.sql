748a4a81efe5d5caf9358174d3a4080706b4deb4
sargsyansak <sargsyansak@gmail.com>
2019-02-24 18:39:33 +0400

    Web site Social Network

---
+++ social_network.sql
@@ -0,0 +1,80 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.18-log : Database - social_network
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`social_network` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `social_network`;
+
+/*Table structure for table `friend_request` */
+
+DROP TABLE IF EXISTS `friend_request`;
+
+CREATE TABLE `friend_request` (
+  `from_id` int(11) NOT NULL AUTO_INCREMENT,
+  `to_id` int(11) NOT NULL,
+  PRIMARY KEY (`from_id`,`to_id`),
+  KEY `to_id` (`to_id`),
+  CONSTRAINT `friend_request_ibfk_1` FOREIGN KEY (`to_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `friend_request_ibfk_2` FOREIGN KEY (`from_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `message` */
+
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_id` int(11) NOT NULL,
+  `to_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `send_date` datetime NOT NULL,
+  `file` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`,`from_id`,`to_id`),
+  KEY `from_id` (`from_id`),
+  KEY `to_id` (`to_id`),
+  CONSTRAINT `message_ibfk_2` FOREIGN KEY (`from_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `message_ibfk_3` FOREIGN KEY (`to_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `pic_url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user_friend` */
+
+DROP TABLE IF EXISTS `user_friend`;
+
+CREATE TABLE `user_friend` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `friend_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`friend_id`),
+  KEY `friend_id` (`friend_id`),
+  CONSTRAINT `user_friend_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `user_friend_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
